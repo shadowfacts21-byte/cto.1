0d371fb1303be5697043017a4997b717023e5572
Backend Engineer <backend-engineer@example.com>
2026-05-07 18:45:47 +0000

    Scaffold project: Add Architecture and Workflow

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
-});
